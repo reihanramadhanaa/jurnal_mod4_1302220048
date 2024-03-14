d7aa8dc7fc51d29a6d660d27ea7865ab66807efe
unknown <ramadhanaa44@gmail.com>
2024-03-14 10:18:03 +0700

    menambahkan class KodeBuah

---
+++ modul4_1302220048/KodeBuah.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302220048
+{
+    internal class KodeBuah
+    {
+        private string[,] Kodebuah = {
+         {"Apel","A00" },
+         {"Aprikot","B00" },
+         {"Alpukat","C00" },
+         {"Pisang","D00" },
+         {"Paprika","E00" },
+         {"Blacberry","F00"},
+         {"Ceri","H00"},
+         {"Kelapa","I00"},
+         {"Jagung","J00"},
+         {"Apel","A00"},
+         {"Aprikot","B00"},
+         {"Aprikot","B00"},
+
+
+        };
+
+        public string GetKodeBuah(string buah) 
+        {
+          for (int i = 0; i < Kodebuah.GetLength(0);i++)
+            {
+                if (Kodebuah[i,0] == buah) 
+                {
+                  return Kodebuah[i,1];
+                }
+            }
+            return "Kode Buah Tidak Ditemukan";
+        
+        }
+
+
+    }
+}
---
+++ modul4_1302220048/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        modul4_1302220048.KodeBuah kodeBuahObj = new modul4_1302220048.KodeBuah();
+        string kodeBuahKelapa = kodeBuahObj.GetKodeBuah("Kelapa");
+        Console.WriteLine( "Kode Buah Kelapa :"  + kodeBuahKelapa);
+
+    }
+}
